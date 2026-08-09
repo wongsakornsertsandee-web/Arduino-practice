f813d416f5e401f41133e3d51d7971e81275681f
Wongsakorn Sertsandee <wongsakorn.sertsandee@gmail.com>
2026-08-09 22:49:13 +0300

    Operator practice part 1
    Practice using integer variables and the addition operator.
    Print the initial values and calculated sum to the Serial Monitor.

---
+++ 01-operators/operator-part-1.ino
@@ -0,0 +1,25 @@
+#include <Arduino.h> // Include the Arduino library for serial communication
+
+int sum = 0; // Initialize an integer variable to store the sum
+int data_1 = 1; // Initialize an integer variable with the value 1
+int data_2 = 2; // Initialize an integer variable with the value 2
+
+
+void setup () {
+  Serial.begin(9600); // Initialize serial communication at 9600 baud rate
+  
+  Serial.println("Setup complete. Starting loop...");
+
+  Serial.print("Sum: "); Serial.println(sum); // Print the initial sum
+  Serial.print("Data 1: "); Serial.println(data_1); // Print the value of data_1
+  Serial.print("Data 2: "); Serial.println(data_2); // Print the value of data_2  
+
+
+  sum = data_1 + data_2; // Calculate the sum of data_1 and data_2
+  Serial.print("Total Sum: "); Serial.println(sum); // Print the new sum
+
+}
+
+void loop () {
+  // Your main code here
+}
